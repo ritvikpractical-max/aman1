2ee727fcb7865711747d1f80a362f571ca834c59
aman <aman1357@gmail.com>
2026-08-13 09:55:50 +0530

    first

---
+++ lk.py
@@ -0,0 +1,13 @@
+char = input ("Enter a letter: ")
+if char =='a':
+    print("Vowel")
+    elif char == 'e':
+        print("Vowel")
+     elif char == 'i':
+        print("Vowel")
+     elif char =='o':
+        print("Vowel")
+    elif char == 'u':
+        print("Vowel")
+else 
+print("Not a vowel")   
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+First_number = int(input("Enter the first number:"))
+second_number = int(input("Enter the second number:"))
+
+
+
+if (first_number >second_number):
+print("The greater number is:{First_number}")
+elif (second_number > First_number):
+    print("The greater number is: {second_number}")
+    else 
+    print("Both numbers are equal.")
