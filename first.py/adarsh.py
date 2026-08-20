e4e4685e37300b002c56a892bbf472abf4b3ff64
aman <aman1357@gmail.com>
2026-08-20 09:49:05 +0530

    20-0826

---
+++ first.py/adarsh.py
@@ -0,0 +1,10 @@
+colour_light = input("enter colour of traffic light")
+
+if colour_light=="red":
+    print("stop")
+elif colour_light=="yellow":
+    print("ready")
+elif colour_light=="green":
+    print("go")
+else:
+     print("invalid")
